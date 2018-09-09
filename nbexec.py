7477085fe8fffcaf0b939eb9270e837ab4d90c9e
Martin Skarzynski <marskar@gmail.com>
2018-09-09 14:57:50 -0400

    update main scripts from biof309 repo

--- nbexec.py
+++ nbexec.py
@@ -1,32 +1,43 @@
-import argparse
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
-parser = argparse.ArgumentParser(
-    description='Execute a notebook from the command line.')
 
-parser.add_argument('source', help='The filename of the input notebook.')
+def nbexec(input_name: str,
+           output_name: str = 'out.ipynb',
+           output_path: str = './') -> None:
 
-parser.add_argument('--out', '-o', default='output.ipynb',
-                    help='The filename of the output notebook.')
+    with open(input_name) as f:
+        nb = nbformat.read(f, as_version=4)
 
-parser.add_argument('--path', '-p', default='.',
-                    help='The filepath where the output notebook is saved.')
+    ep = ExecutePreprocessor()
 
-args = parser.parse_args()
-input_filename = args.source
-output_filename = args.out
-filepath = args.path
+    ep.preprocess(nb, {'metadata': {'path': output_path}})
 
-if not filepath.endswith('/'):
-    filepath += '/'
+    if not output_path.endswith('/'):
+        output_path += '/'
 
-with open(input_filename) as f:
-    nb = nbformat.read(f, as_version=4)
+    with open(output_path+output_name, 'wt') as f:
+        nbformat.write(nb, f)
 
-ep = ExecutePreprocessor()
 
-ep.preprocess(nb, {'metadata': {'path': filepath}})
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Execute a notebook from the command line.')
 
-with open(filepath + output_filename, 'wt') as f:
-    nbformat.write(nb, f)
+    parser.add_argument('source', help='The filename of the input notebook.')
+
+    parser.add_argument('--out', '-o', default='out.ipynb',
+                        help='The filename of the output notebook.')
+
+    parser.add_argument('--path', '-p', default='.',
+                        help='The filepath where the output notebook is saved.')
+
+    args = parser.parse_args()
+    in_name = args.source
+    out_name = args.out
+    out_path = args.path
+
+    nbexec(input_name=in_name,
+           output_name=out_name,
+           output_path=out_path)
--- nbuild.py
+++ nbuild.py
@@ -1,38 +1,49 @@
-import argparse
 import nbformat
-
-parser = argparse.ArgumentParser(
-    description='Create a notebook from the command line.')
-
-parser.add_argument('names', nargs='+', help='A series of filenames.')
-
-parser.add_argument('--out', '-o', default='raw.ipynb',
-                    help='The filename of the output notebook.')
-
-parser.add_argument('--path', '-p', default='./',
-                    help='The filepath where the output notebook is saved.')
-
-args = parser.parse_args()
-filenames = args.names
-output_filename = args.out
-filepath = args.path
-
-if not filepath.endswith('/'):
-    filepath += '/'
+from typing import List
 
 
-def read_file(filename):
-    with open(filename) as f:
-        return f.read()
+def nbuild(filenames: List,
+           output_name: str = "raw.ipynb",
+           output_path: str = './') -> None:
+
+    def read_file(filename):
+        with open(filename) as f:
+            return f.read()
+
+    nb = nbformat.v4.new_notebook()
+    md_cell = nbformat.v4.new_markdown_cell
+    code_cell = nbformat.v4.new_code_cell
+
+    nb['cells'] = [code_cell(read_file(name))
+                   if name.endswith('.py')
+                   else md_cell(read_file(name))
+                   for name in filenames]
+
+    if not output_path.endswith('/'):
+        output_path += '/'
+
+    nbformat.write(nb, output_path+output_name)
 
 
-nb = nbformat.v4.new_notebook()
-md_cell = nbformat.v4.new_markdown_cell
-code_cell = nbformat.v4.new_code_cell
+if __name__ == "__main__":
+    import argparse
 
-nb['cells'] = [code_cell(read_file(name))
-               if name.endswith('.py')
-               else md_cell(read_file(name))
-               for name in filenames]
+    parser = argparse.ArgumentParser(
+        description='Create a notebook from the command line.')
 
-nbformat.write(nb, filepath + output_filename)
+    parser.add_argument('names', nargs='+', help='A series of filenames.')
+
+    parser.add_argument('--out', '-o', default='raw.ipynb',
+                        help='The filename of the output notebook.')
+
+    parser.add_argument('--path', '-p', default='./',
+                        help='The path where the output notebook is saved.')
+
+    args = parser.parse_args()
+    names = args.names
+    out_name = args.out
+    out_path = args.path
+
+    nbuild(filenames=names,
+           output_name=out_name,
+           output_path=out_path)
