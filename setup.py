edcfa48335936e3859a354213a720ff21b5e4a20
Martin Skarzynski <marskar@gmail.com>
2018-09-10 19:50:01 -0400

    deployed nbless to PyPI

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbless",
+    version="0.0.1",
+    author="Martin Skarzynski",
+    author_email="marskar@gmail.com",
+    description="Create Jupyter notebooks from Markdown and Python scripts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marskar/nbless",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
