2cda8e985edda4abdbc35181cb55f3fa6d53acc0
Martin Skarzynski <marskar@gmail.com>
2018-09-10 20:19:57 -0400

    fixed imports

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 #from .nbless import nbless
-from .nbuild import nbuild
-from .nbexec import nbexec
+from nbuild import nbuild
+from nbexec import nbexec
 
 __all__ = ['nbless', 'nbuild', 'nbexec']
 
