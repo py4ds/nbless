977a8df5202ecd5f7704f51bd969597366a47c32
Martin Skarzynski <marskar@gmail.com>
2018-09-17 14:34:41 -0400

    update catrmd example in docs and stable version 010

--- build/lib/nbless/__init__.py
+++ build/lib/nbless/__init__.py
@@ -1,7 +1,6 @@
-from nbless import nbless
-from nbless import nbuild
-from nbless import nbexec
-from nbless import catrmd
+from nbless.nbuild import nbuild
+from nbless.nbexec import nbexec
+from nbless.nbless import nbless
+from nbless.catrmd import catrmd
 
 __all__ = ['nbless', 'nbuild', 'nbexec', 'catrmd']
-
--- nbless/__init__.py
+++ nbless/__init__.py
@@ -1,6 +1,6 @@
+from nbless.nbuild import nbuild
+from nbless.nbexec import nbexec
 from nbless.nbless import nbless
-from nbless.nbless import nbuild
-from nbless.nbless import nbexec
-from nbless import catrmd
+from nbless.catrmd import catrmd
 
 __all__ = ['nbless', 'nbuild', 'nbexec', 'catrmd']
