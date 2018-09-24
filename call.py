ed70866675ce295e8ec1c1ebaef2152e179d9591
Martin Skarzynski <marskar@gmail.com>
2018-09-23 22:57:12 -0400

    add script to demo calls to rmarkdown and nbconvert

---
+++ call.py
@@ -0,0 +1,6 @@
+from subprocess import call
+
+call('ls ' +  '-l', shell=True)
+call('Rscript -e \'rmarkdown::render("cat.Rmd", "all")\'', shell=True)
+call('jupyter nbconvert --to notebook --execute raw.ipynb --output out.ipynb', shell=True)
+
