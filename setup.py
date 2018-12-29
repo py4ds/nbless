e6f3cfff6a800750c91d132e6e177c88bbcc02ec
Martin Skarzynski <marskar@gmail.com>
2018-12-28 23:44:50 -0500

    deploy 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nbless",
-    version="0.2.0",
+    version="0.2.1",
     author="Martin Skarzynski",
     author_email="marskar@gmail.com",
     description="Create Jupyter notebooks from Markdown and Python scripts.",
