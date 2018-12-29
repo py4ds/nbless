6c9ff4a76033573e5f4ce0dcae96429dd8f4c6f8
Martin Skarzynski <marskar@gmail.com>
2018-12-28 20:48:28 -0500

    try to simplify imports via __init__.py

--- src/nbless/__init__.py
+++ src/nbless/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ['nbless', 'nbuild', 'nbexec', 'nbcode', 'nbhtml']
+from src import nbless
+from src.nbless.nbless import nbless
+from src.nbless.nbuild import nbuild
+from src.nbless.nbexec import nbexec
+from src.nbless.nbconv import nbconv
+
+__all__ = ['nbless', 'nbuild', 'nbexec', 'nbconv']
