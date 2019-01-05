c9f846be2be08949ceef7d1573efe63cc051e973
Martin Skarzynski <marskar@gmail.com>
2019-01-04 21:57:53 -0500

    fix setup.py readme reference

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -10,7 +10,7 @@ setuptools.setup(
     author_email="marskar@gmail.com",
     description="Create Jupyter notebooks from Markdown and Python scripts.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/marskar/nbless",
     package_dir={"": "src"},
     packages=setuptools.find_packages('src'),
