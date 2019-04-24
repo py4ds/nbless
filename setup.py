13203885f777923546319946e6da7507fc787e28
Martin Skarzynski <marskar@gmail.com>
2019-04-24 13:00:58 -0400

    Bump version to 0.2.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="nbless",
-    version="0.2.18",
+    version="0.2.19",
     author="Martin Skarzynski",
     author_email="marskar@gmail.com",
     description="Create Jupyter notebooks from Markdown and Python scripts.",
