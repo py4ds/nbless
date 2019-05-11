b4b1c1cea6c0033b137c3f6d4f427203dc7a6102
Martin Skarzynski <marskar@gmail.com>
2019-05-10 21:31:56 -0400

    add nbdeck function and cli

---
+++ src/cli/nbdeck_click.py
@@ -0,0 +1,12 @@
+import click
+
+from nbless.nbdeck import nbdeck
+from nbless.nbsave import nbsave
+
+
+@click.command()
+@click.argument("in_file")
+@click.option("-o", "--out_file", "out")
+def nbexec_click(in_file: str, out: str) -> None:
+    """Set up a notebook to be viewed as or converted into slides."""
+    nbsave(out, nbdeck(in_file)) if out else nbsave(in_file, nbdeck(in_file))
---
+++ src/nbless/nbdeck.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+import nbformat
+
+
+def nbdeck(filename: str) -> nbformat.notebooknode.NotebookNode:
+    """ Set up a Jupyter notebook to be viewed as or converted into slides.
+
+    Sets `slide_type` to ``slide`` for markdown cells that start with headers.
+    After running ``nbdeck``, you can
+    - view Jupyter notebooks on nbviewer or with the RISE extension
+    - create an HTML slideshow with ``nbconv`` or ``jupyter nbconvert``.
+
+    :param filename: The filename of the input jupyter notebook.
+    """
+    nb = nbformat.read(filename, as_version=4)
+    for n, cell in enumerate(nb.cells):
+        if cell.cell_type == "markdown" and cell.source.startswith("#"):
+            nb.cells[n].slide_type = "slide"
+    return nb
