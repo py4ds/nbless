a2e781ca2322c50cf044dae0b18e05614123ac76
Martin Skarzynski <marskar@gmail.com>
2019-05-10 21:41:37 -0400

    fix out arg in nbexec_click

--- src/cli/nbexec_click.py
+++ src/cli/nbexec_click.py
@@ -7,7 +7,7 @@ from nbless.nbsave import nbsave
 @click.command()
 @click.argument("in_file")
 @click.option("-k", "--kernel", "kernel")
-@click.option("-o", "--out_file", "out_file")
+@click.option("-o", "--out_file", "out")
 def nbexec_click(in_file: str, kernel: str, out: str) -> None:
     """Create an executed notebook without modifying the input notebook."""
     nb_name, nb = nbexec(in_file, kernel) if kernel else nbexec(in_file)
