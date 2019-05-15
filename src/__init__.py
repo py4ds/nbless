fcd4579b0f6af19fe3fd48fa70a19e0ba2866688
Martin Skarzynski <marskar@gmail.com>
2019-05-14 23:19:10 -0400

    simplify import using __init__.py

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from nbless.nbless import nbless
-from nbless.nbuild import nbuild
-from nbless.nbexec import nbexec
-from nbless.nbconv import nbconv
-from nbless.nbdeck import nbdeck
-from nbless.nbraze import nbraze
-
-__all__ = ["nbless", "nbuild", "nbexec", "nbconv", "nbdeck", "nbraze"]
--- src/nbless/__init__.py
+++ src/nbless/__init__.py
@@ -1,1 +1,9 @@
 """Nbless: a Python package for programmatic Jupyter notebook workflows."""
+from nbless.nbless import nbless
+from nbless.nbuild import nbuild
+from nbless.nbexec import nbexec
+from nbless.nbconv import nbconv
+from nbless.nbdeck import nbdeck
+from nbless.nbraze import nbraze
+
+__all__ = ["nbless", "nbuild", "nbexec", "nbconv", "nbdeck", "nbraze"]
