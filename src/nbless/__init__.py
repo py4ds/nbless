116f5800c2ac618012296d167b25c92cacdfd637
Martin Skarzynski <marskar@gmail.com>
2019-06-19 14:00:17 -0400

    Bump version: 0.2.30 → 0.2.31

--- src/nbless/__init__.py
+++ src/nbless/__init__.py
@@ -7,5 +7,5 @@ from nbless.nbdeck import nbdeck
 from nbless.nbraze import nbraze
 
 __author__ = "Martin Skarzynski"
-__version__ = "__version__ = '0.2.30'"
+__version__ = "__version__ = '0.2.31'"
 __all__ = ["nbless", "nbuild", "nbexec", "nbconv", "nbdeck", "nbraze"]
