55f55ace14d181ff2d55f0725d1a4b4315b61406
Martin Skarzynski <marskar@gmail.com>
2019-06-19 13:50:42 -0400

    Bump version: 0.2.28 → 0.2.29

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, Martin Skarzynski'
 author = 'Martin Skarzynski'
 
 # The short X.Y version
-version = '0.2.28'
+version = '0.2.29'
 # The full version, including alpha/beta/rc tags
 release = version
 
